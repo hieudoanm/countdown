c5b1621a9f0d83d73b8e372f9f633021e3735cbc
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-12 20:48:18 +0700

    add cap

--- docs/_next/static/chunks/cba89f957b34b389.js
+++ docs/_next/static/chunks/cba89f957b34b389.js
@@ -3,7 +3,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/3078a683fcd22156.js",
   "static/chunks/59f4b729df0502c1.js",
   "static/chunks/b4f160c7cf54ef8e.js",
-  "static/chunks/99ffaec19b30f400.css",
+  "static/chunks/8b4ec0d26c0f5b31.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-95e5f3332b08fedc.js"
+  "static/chunks/turbopack-0bf9537a3f623f78.js"
 ])
